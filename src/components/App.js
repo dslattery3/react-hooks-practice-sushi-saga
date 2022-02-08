880916789cdbdc1fd3eebce756338e1fde48524b
Dalton <slattery.da@gmail.com>
2022-02-08 17:42:46 -0600

    post after party w/ adam

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [wallet, setWallet] = useState(100)
+  const [tummy, setTummy] = useState([])
+ 
+  useEffect(() => {
+    fetch(API).then(r=>r.json()).then(setSushis) }, [])
+
+  const eatSushi = sushi => {
+
+    if(wallet >= sushi.price){
+      setWallet(wallet - sushi.price)
+
+      const newSushis = [...sushis]
+      const index = newSushis.findIndex(s => s.id === sushi.id)
+      newSushis[index].eaten = true
+      setSushis(newSushis)
+
+      const newTummy = [...tummy]
+      newTummy.push(sushi)
+      setTummy(newTummy)
+    }
+  }
+
+  const gamble = () => {
+      let newMoney = Math.floor(Math.random() * 100)
+      if (newMoney > 80 || newMoney < 20){
+        newMoney = newMoney * -1
+      }
+      setWallet(wallet + newMoney)
+    }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} eatSushi={eatSushi} gamble={gamble}/>
+      <Table wallet={wallet} plates={tummy}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, eatSushi, gamble}) {
+  const [display, setDisplay] = useState(0)
+  const addFour = () => {
+    const newIndex = display + 4
+    if (!sushis[newIndex +4]){
+      setDisplay(0)
+    }
+    else{
+      setDisplay(newIndex)
+    }
+  }
+
+  const fourToShow = sushis.slice(display, display + 4)
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      <button onClick ={gamble}>More Money</button>
+      {fourToShow.map((sushi) => <Sushi key={sushi.id} eatSushi={eatSushi} sushi={sushi}/>)}
+      <MoreButton addFour={addFour}/>
     </div>
   );
 }
